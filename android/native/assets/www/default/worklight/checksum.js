b0b92b96c99b3ddaa61b8a1d29c4295e8c6d3d80
Mac Developer <macdeveloper@Gabriels-MacBook-Pro.local>
2016-02-25 16:57:13 -0600

    Icons Apps and DST Permissions

--- android/native/assets/www/default/worklight/checksum.js
+++ android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1456439387750,"machine":"9.86.114.40","checksum":2682441769};
-/* Date: Thu Feb 25 16:29:47 CST 2016 */
+var WL_CHECKSUM = {"date":1456440992809,"machine":"9.86.114.40","checksum":982614230};
+/* Date: Thu Feb 25 16:56:32 CST 2016 */
--- iphone/native/www/default/worklight/checksum.js
+++ iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1456439373400,"machine":"9.86.114.40","checksum":282194572};
-/* Date: Thu Feb 25 16:29:33 CST 2016 */
+var WL_CHECKSUM = {"date":1456440991123,"machine":"9.86.114.40","checksum":938123722};
+/* Date: Thu Feb 25 16:56:31 CST 2016 */
