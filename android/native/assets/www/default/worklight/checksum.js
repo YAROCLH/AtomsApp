d4d3344999b96bc1a25caf94f08e826ba59ee336
Mac Developer <macdeveloper@Gabriels-MacBook-Pro.local>
2016-03-02 13:38:01 -0600

    reparacion de rank

--- android/native/assets/www/default/worklight/checksum.js
+++ android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1456852476945,"machine":"9.86.117.84","checksum":3398555559};
-/* Date: Tue Mar 01 11:14:36 CST 2016 */
+var WL_CHECKSUM = {"date":1456933041288,"machine":"9.86.116.18","checksum":3398555559};
+/* Date: Wed Mar 02 09:37:21 CST 2016 */
--- iphone/native/www/default/worklight/checksum.js
+++ iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1456852502092,"machine":"9.86.117.84","checksum":557753964};
-/* Date: Tue Mar 01 11:15:02 CST 2016 */
+var WL_CHECKSUM = {"date":1456933043940,"machine":"9.86.116.18","checksum":557753964};
+/* Date: Wed Mar 02 09:37:23 CST 2016 */
