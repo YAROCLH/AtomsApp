29698a4bcd2545b38135e65546b57c21b8bd2b6c
Mike325 <miguel.8a.hdez@gmail.com>
2016-03-06 18:43:38 -0600

    eliminada funcion duplicada

--- common/js/controllers/myPoints.js
+++ common/js/controllers/myPoints.js
@@ -13,11 +13,3 @@
 /**
  * Functions
  */
-			function init_myPoints(){
-				var newScore = parseInt(currentScore) + parseInt(selectedScore);
-				document.getElementById("scoreAfter").innerHTML = currentScore;
-				document.getElementById("lastNewScore").innerHTML = "+" + selectedScore;
-				document.getElementById("finalScore").innerHTML = newScore.toString();
-			}
-			
-			
