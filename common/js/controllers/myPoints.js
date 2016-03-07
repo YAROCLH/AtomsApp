4c3563ec971efabdd9d9f54e885022f34267b724
Mike325 <miguel.8a.hdez@gmail.com>
2016-03-06 18:30:12 -0600

    La pantalla de congratulations ya aparece siempre

--- common/js/controllers/myPoints.js
+++ common/js/controllers/myPoints.js
@@ -8,21 +8,16 @@
  */
 	  		$(document).ready(function(){
 	  			myPoints_js=true;
-				init_myPoints();
 			});
-	  		$("#closeMyPoints").on("click",function(){
-	  			setView("category", true, true);
-	  		});
+
 /**
  * Functions
  */
 			function init_myPoints(){
-				var newScore=parseInt(currentScore)+parseInt(selectedScore);
-				console.log("newScore"+newScore);
-				$(".oldScore").text(currentScore);
-				$(".addScore").text("+"+selectedScore);
-				$(".newScore").text(newScore.toString());
-				
+				var newScore = parseInt(currentScore) + parseInt(selectedScore);
+				document.getElementById("scoreAfter").innerHTML = currentScore;
+				document.getElementById("lastNewScore").innerHTML = "+" + selectedScore;
+				document.getElementById("finalScore").innerHTML = newScore.toString();
 			}
 			
 			
