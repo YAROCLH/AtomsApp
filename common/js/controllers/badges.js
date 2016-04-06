2cdcc7f8a757417e81fb19db384f8da19124a524
Miguel <miguel.8a.hdez@gmail.com>
2016-04-06 10:58:33 -0500

    eliminados comentarios y agregada URL de los badges

--- common/js/controllers/badges.js
+++ common/js/controllers/badges.js
@@ -21,40 +21,37 @@
 		function init_badges(){
 			badges_js=true;
 		}
+        
 
+        /*
+         *
+         *
+         **/
         function setBadgePercentage()
         {
-
 			$.when(get_Data(Badges_Json)).then(function(badges_data)
             {
-                //var badges_points = badges_data;
-                // console.log("badges obtenidos");
                 var badges_points = [badges_data[0].L1, badges_data[0].L2, badges_data[0].L3, badges_data[0].L4];
-                // console.log("total del badge " + badges_points.length);
+                // var badges_points = [];
+                // for (var i in badges_data[0]) {
+                //     badges_points.push(i);
+                // }
+
                 var data_status = "idUser="+encodeString(global_UserId);
                 var categories, score, newPercent;
                 
-                /*
-                 var badges_points = [];
-                 for(var i in badges_data[0])
-                 {
-                    badges_points.add(parseInt(i));
-                 }
-                 **/
-
                 $.when(get_Data(Status_Json,data_status)).then(function(status_data)
                 {
-                    // console.log("status obtenido");
 
                      categories           = status_data;
                     var number_categories = categories.length;
                     var category          = 0;
-                    var catID             = ['#Cat1', '#Cat2', '#Cat3', '#Cat4', '#Cat5' ];
                     var catID_counter     = 0;
+
+                    var catID             = ['#Cat1', '#Cat2', '#Cat3', '#Cat4', '#Cat5' ];
 
                     jQuery('.title5').each(function()
                     {
-                        // console.log("categoria #" + category);
                         var total = parseInt(categories[category].Total);
                         if( category >= number_categories )
                         {
@@ -67,12 +64,9 @@
                             var badge_total = 0; 
                             var prev_badge  = 0;
                             
-                            // console.log("score " + score);
                             
                             for(var i = 0; i < badges_points.length; i++)
                             {
-                                // console.log("Iteracion " + i);
-                                // console.log("puntos del badge " + badges_points[i]);
                                 var badge    = parseInt(badges_points[i]);
                                 badge_total += badge; 
                                 
@@ -81,14 +75,10 @@
                                     prev_badge += parseInt(badges_points[i - 1]);
                                 }
 
-                                // console.log("Total Badges    " + badge_total );
-                                // console.log("Total preBadges " + prev_badge);
                                 if( score <= badge_total )
                                 {
                                     prev_score = score - prev_badge;
                                     newPercent = prev_score/badge * 100; 
-                                    // console.log("prev_score " + prev_score);
-                                    // console.log("newPercent " + newPercent);
                                     break;
                                 }
                             }
@@ -97,33 +87,27 @@
                         newPercent        = parseInt(newPercent);   	
                         var badge_counter = 0;
                         var badge_total   = 0;
+                        var prev_badge    = 0;
                         var last_badge    = false;
-                        // console.log("Cat ID " + catID[category]); 
-                        // console.log("Score " + score);
 
                         jQuery(catID[category]).find('.img-responsive').each(function()
                         {
                             if( !last_badge )
                             {
-
-                                // console.log("iteracion de imagenes #" + badge_counter);
-                                // console.log("no ha terminado de colorear");
-                                // console.log("Puntos de badge " + badges_points[badge_counter]); 
+                                prev_badge  += badge_total;
                                 badge_total += parseInt(badges_points[badge_counter]);
 
-                                // console.log("Total " +badge_total + " hasta " + badge_counter);
 
                                 if( score > badge_total )
                                 {
-                                    //jQuery(this).removeClass
-                                    // colorea todo
-                                    jQuery(this).removeClass("img-inactive", 500);
+                                    jQuery(this).removeClass("img-inactive");
                                 }
                                 else 
                                 {
                                     last_badge = true;
-                                     
-                                    // colorea con newpersent
+                                    score -= prev_badge; 
+                                    newPercent = score/badges_points[badge_counter] * 100;
+                                    // colorear parte
                                 }
                                 badge_counter++;
                             }
