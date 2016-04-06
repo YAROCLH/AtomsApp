db471c50f41f3370de4b5d45e543cc0b672354c9
Miguel <miguel.8a.hdez@gmail.com>
2016-04-06 10:31:38 -0500

    primera iteracion de badges

--- common/js/controllers/badges.js
+++ common/js/controllers/badges.js
@@ -8,6 +8,7 @@
  */
 		$(document).ready(function() {
 			init_badges();
+			setBadgePercentage();
 		});
 		
 		$(document).on('click', "#status",function(){
@@ -20,3 +21,116 @@
 		function init_badges(){
 			badges_js=true;
 		}
+
+        function setBadgePercentage()
+        {
+
+			$.when(get_Data(Badges_Json)).then(function(badges_data)
+            {
+                //var badges_points = badges_data;
+                // console.log("badges obtenidos");
+                var badges_points = [badges_data[0].L1, badges_data[0].L2, badges_data[0].L3, badges_data[0].L4];
+                // console.log("total del badge " + badges_points.length);
+                var data_status = "idUser="+encodeString(global_UserId);
+                var categories, score, newPercent;
+                
+                /*
+                 var badges_points = [];
+                 for(var i in badges_data[0])
+                 {
+                    badges_points.add(parseInt(i));
+                 }
+                 **/
+
+                $.when(get_Data(Status_Json,data_status)).then(function(status_data)
+                {
+                    // console.log("status obtenido");
+
+                     categories           = status_data;
+                    var number_categories = categories.length;
+                    var category          = 0;
+                    var catID             = ['#Cat1', '#Cat2', '#Cat3', '#Cat4', '#Cat5' ];
+                    var catID_counter     = 0;
+
+                    jQuery('.title5').each(function()
+                    {
+                        // console.log("categoria #" + category);
+                        var total = parseInt(categories[category].Total);
+                        if( category >= number_categories )
+                        {
+                            newPercent = 0;
+                            score      = 0;
+                        }
+                        else
+                        {
+                            score           = parseInt(categories[category].Score);
+                            var badge_total = 0; 
+                            var prev_badge  = 0;
+                            
+                            // console.log("score " + score);
+                            
+                            for(var i = 0; i < badges_points.length; i++)
+                            {
+                                // console.log("Iteracion " + i);
+                                // console.log("puntos del badge " + badges_points[i]);
+                                var badge    = parseInt(badges_points[i]);
+                                badge_total += badge; 
+                                
+                                if( i > 0)
+                                {
+                                    prev_badge += parseInt(badges_points[i - 1]);
+                                }
+
+                                // console.log("Total Badges    " + badge_total );
+                                // console.log("Total preBadges " + prev_badge);
+                                if( score <= badge_total )
+                                {
+                                    prev_score = score - prev_badge;
+                                    newPercent = prev_score/badge * 100; 
+                                    // console.log("prev_score " + prev_score);
+                                    // console.log("newPercent " + newPercent);
+                                    break;
+                                }
+                            }
+                        }
+
+                        newPercent        = parseInt(newPercent);   	
+                        var badge_counter = 0;
+                        var badge_total   = 0;
+                        var last_badge    = false;
+                        // console.log("Cat ID " + catID[category]); 
+                        // console.log("Score " + score);
+
+                        jQuery(catID[category]).find('.img-responsive').each(function()
+                        {
+                            if( !last_badge )
+                            {
+
+                                // console.log("iteracion de imagenes #" + badge_counter);
+                                // console.log("no ha terminado de colorear");
+                                // console.log("Puntos de badge " + badges_points[badge_counter]); 
+                                badge_total += parseInt(badges_points[badge_counter]);
+
+                                // console.log("Total " +badge_total + " hasta " + badge_counter);
+
+                                if( score > badge_total )
+                                {
+                                    //jQuery(this).removeClass
+                                    // colorea todo
+                                    jQuery(this).removeClass("img-inactive", 500);
+                                }
+                                else 
+                                {
+                                    last_badge = true;
+                                     
+                                    // colorea con newpersent
+                                }
+                                badge_counter++;
+                            }
+                        });
+
+                        category++; 
+                    });
+                });
+            });
+        }
--- common/js/controllers/status.js
+++ common/js/controllers/status.js
@@ -6,48 +6,62 @@
 /**
  * Events
  */
-		$(document).ready(function(){
+		$(document).ready(function()
+		{
 			init_status();
 		});
 /**
  * Functions
  */
-		function init_status(){
+		function init_status()
+        {
 			status_js=true;
 			var data_status="idUser="+global_UserId;
 			Status_BackButton=$(".ClickBackBad");
 			Animate();
-			
 		}
 		
-		$(Status_BackButton).on("click",function(){
-			  setView("badges",badges_js,false);
+		$(Status_BackButton).on("click",function()
+		{
+            setView("badges",badges_js,false);
 		});
 		
-		function Animate(){
-			 var category    = 0;						
-		     var totals      = [115, 16, 12, 16, 18];	
-		     var percents    = ['percent1', 'percent2', 'percent3', 'percent4', 'percent5'];
-		     var data_status ="idUser="+encodeString(global_UserId);
-		     var categories;var score;var newPercent;
-			 $.when(get_Data(Status_Json,data_status)).then(function(status_data){
-				categories  = status_data;
-				var t=categories.length;
-				jQuery('.skillbar').each(function(){
-				if(category>=t){
-					newPercent=0;
-				}else{
-					score=parseInt(categories[category].Score);
-					newPercent =score/totals[category] * 100; 
-				}
-					newPercent=parseInt(newPercent);   	
-		            document.getElementById(percents[category]).innerHTML =  newPercent.toString() + "%";
-		            jQuery(this).attr('data-percent', newPercent.toString() + "%");
-		            jQuery(this).find('.skillbar-bar').animate({
-		                width:jQuery(this).attr('data-percent')
-		            },3000);
-		            
-		            category++; 
+		function Animate()
+        {
+			var category    = 0;						
+		    var percents    = ['percent1', 'percent2', 'percent3', 'percent4', 'percent5'];
+		    var data_status = "idUser="+encodeString(global_UserId);
+		    
+		    var categories, score, newPercent;
+
+			$.when(get_Data(Status_Json,data_status)).then(function(status_data)
+            {
+			    categories            = status_data;
+				var number_categories = categories.length;
+
+				jQuery('.skillbar').each(function()
+				{
+                    var total = parseInt(categories[category].Total);
+
+                    if( category >= number_categories )
+                    {
+                        newPercent=0;
+                    }
+                    else
+                    {
+                        score=parseInt(categories[category].Score);
+                        newPercent =score/total * 100; 
+                    }
+
+                    newPercent = parseInt(newPercent);   	
+                    document.getElementById(percents[category]).innerHTML =  newPercent.toString() + "%";
+                    jQuery(this).attr('data-percent', newPercent.toString() + "%");
+
+                    jQuery(this).find('.skillbar-bar').animate({
+                        width:jQuery(this).attr('data-percent')
+                    },3000);
+                    
+                    category++; 
 		        });
 		    });
 		}
